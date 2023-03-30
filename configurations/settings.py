c0c7d6a80d6e23ed7334ea8166053c99fb9363db
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-03-30 10:16:06 +0300

    Reverted to my initial state

--- configurations/settings.py
+++ configurations/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4_9-*#36pk@p&+x@3@cw4j-74o-_h!+3)4&e8^9=$7*$srru5x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -121,11 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [BASE_DIR / 'configurations/static']
-STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')

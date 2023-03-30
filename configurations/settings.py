6358d8e3016a3320a459d9d1c0f2cfb2d0c9ba26
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-03-30 10:26:28 +0300

    App reconfigured to facillitate hosting

--- configurations/settings.py
+++ configurations/settings.py
@@ -127,5 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [BASE_DIR / 'configurations/static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 # STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
